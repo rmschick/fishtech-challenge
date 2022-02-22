75f38eaa72d732043fe4f2d2f065581e2a54b741
Ryan Schick <schick.ryan@yahoo.com>
2022-02-22 14:50:33 -0600

    terraform setup and auto deployment added

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo -e "\n+++++ Starting deployment +++++\n"
+
+tfswitch 1.0.0
+
+rm -rf ./bin
+
+echo "+++++ build go packages +++++"
+
+cd source/
+env GOARCH=amd64 GOOS=linux go build -ldflags="-s -w" -o bin/ipaddress main.go
+cd ..
+
+echo "+++++ ipaddress module +++++"
+cd infrastructure/
+terraform init -input=false
+terraform apply -input=false -auto-approve
+cd ../
+
+echo -e "\n+++++ Deployment done +++++\n"
